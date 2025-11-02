9f1530edea17d71a3f002dd637266ffb50c3c5c6
ayad imad eddine <imday16@gmail.com>
2025-11-02 18:26:29 +0100

    ajoute de la page info et contact

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -75,8 +75,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Adresse mail vérifié, votre compte est activé');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_home');
     }
 }
