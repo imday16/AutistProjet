8369b8ced9fd49571c867af76d162e29b6174267
ayad imad eddine <imday16@gmail.com>
2025-11-06 22:28:08 +0100

    adaptation du userType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +16,43 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('codePostal')
-            ->add('ville')
-            ->add('pays')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,  // permet plusieurs rôles
+                'expanded' => true,  // affiche des cases à cocher
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse',
+                'required' => false,
+            ])
+            ->add('codePostal', TextType::class, [
+                'label' => 'Code postal',
+                'required' => false,
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville',
+                'required' => false,
+            ])
+            ->add('pays', TextType::class, [
+                'label' => 'Pays',
+                'required' => false,
+            ])
         ;
     }
 
